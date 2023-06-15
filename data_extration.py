1665e3e0bec78dc422ba62a8fe912ce02cc41885
EndRay <a.i.kovrigin@gmail.com>
2023-06-15 17:52:55 +0200

    load_data()

--- data_extration.py
+++ data_extration.py
@@ -55,13 +55,16 @@ def extract_data(data_filename, data_amounts, max_chunks_shift=0, data_type='sam
             chunk_sum /= 32768
             match data_type:
                 case 'sample':
-                    result = chunk_sum
+                    result_data = chunk_sum
                 case 'fft':
-                    result = np.fft.fft(chunk_sum)[:CHUNK_SIZE // 2]
+                    result_data = np.fft.fft(chunk_sum)[:CHUNK_SIZE // 2]
+                    result_data = np.abs(result_data)
                 case 'harmonics':
-                    result = note_to_harmonics(44100, chunk_sum, 20)
+                    result_data = note_to_harmonics(44100, chunk_sum, 20)
+                case _:
+                    raise ValueError('Unknown data type')
 
-            data[cnt].append([result, result])
+            data[cnt].append([result_data, result])
 
     print('Data size: ' + str(sum(len(data[i]) for i in range(max_instruments + 1))))
 
@@ -70,3 +73,37 @@ def extract_data(data_filename, data_amounts, max_chunks_shift=0, data_type='sam
         for cnt in range(len(data)):
             for i in range(len(data[cnt])):
                 f.write(" ".join(map(str, data[cnt][i][0])) + "|" + " ".join(map(str, data[cnt][i][1])) + '\n')
+
+
+def load_data(filename, train_test_ratio=0.8):
+    data = [[] for _ in range(len(INSTRUMENTS) + 1)]
+
+    with open(filename, 'r') as f:
+        for line in f:
+            features, answers = line.split('|')
+            print(line)
+            print(line.split('|'))
+            print(line.count('|'))
+            print(features)
+            print(answers)
+
+            features = [float(x) for x in features.split()]
+            answers = [int(x) for x in answers.split()]
+            data_line = [features, answers]
+            cnt = sum(data_line[1])
+            data[cnt].append(data_line)
+
+    train_data = [[] for _ in range(len(INSTRUMENTS) + 1)]
+    test_data = [[] for _ in range(len(INSTRUMENTS) + 1)]
+
+    for i in range(len(INSTRUMENTS) + 1):
+        train_data[i] = data[i][:int(len(data[i]) * train_test_ratio)]
+        test_data[i] = data[i][int(len(data[i]) * train_test_ratio):]
+
+    train_data = [x for y in train_data for x in y]
+    test_data = [x for y in test_data for x in y]
+
+    random.shuffle(train_data)
+    random.shuffle(test_data)
+
+    return train_data, test_data
